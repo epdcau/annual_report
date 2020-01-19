7263d01f8d960ba7af369123f975517d65a8613e
HUGHES Josh L <Josh.L.Hughes@ci.eugene.or.us>
2020-01-18 17:44:48 -0800

    case

---
+++ case.sql
@@ -0,0 +1,37 @@
+declare @year int = 2018
+
+
+select l.inci_id case_id, 
+	c.chrgcnt charge_seq,
+	rtrim(c.chrgdesc) charge, 
+	--(select inci_id from CADReporting.dbo.inmain x where x.case_id = l.inci_id and x.case_id != '' and x.agency = 'EPD' for json path) inci_id,
+	PSJReporting.EugCrimeAnalyst.fix_datetime(l.date_occu, l.hour_occu) occur_dt,
+	rtrim(c.ucr_code) ucr_code,
+	upper(u.ucr_categ) ucr_cat,
+	upper(u.ucr_group) ucr_group,
+	rtrim(l.reportedas),
+	(select rtrim(descriptn) from RMSReporting.dbo.systab2 x where x.code_agcy = l.csstatus and x.code_key = 'CASS') case_status,
+	l.csstatus, 
+	(select case when x.descriptn = 'NO BIAS' then null else x.descriptn end from RMSReporting.dbo.systab2 x where c.hatebias = x.code_agcy and x.code_key = 'MOTI') bias,
+	isnull( (
+		select age, sex, rtrim(name_type) type
+		from RMSReporting.dbo.lwnames x
+		where x.lwmainid = l.lwmainid
+		and replace((',' + replace(x.vic_crime, ' ', ',') + ','), ',,', ',') like '%,' +  cast(c.chrgcnt as varchar(100)) + ',%'
+		and name_id != 0
+		and name_type != 'B'
+		for json path
+	), '[]') vic_lst
+from RMSReporting.dbo.lwmain l
+inner join RMSReporting.dbo.lwchrg c 
+on l.lwmainid = c.lwmainid
+left join PSJReporting.EugCrimeAnalyst.rms_ucr_convert u
+on c.ucr_code = u.ucr_code
+where year(l.date_occu) = @year
+and agency = 'EPD'
+and l.offense not like 'VOIDED%'
+
+
+
+
+
