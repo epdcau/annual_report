a7daaed01a7d97f700f68701a96191d71e92c0d9
HUGHES Josh L <Josh.L.Hughes@ci.eugene.or.us>
2020-01-19 12:36:10 -0800

    update

--- case.sql
+++ case.sql
@@ -10,10 +10,11 @@ select
 	PSJReporting.EugCrimeAnalyst.local_date_string(PSJReporting.EugCrimeAnalyst.fix_datetime(l.date_occu, l.hour_occu)) occur_dt,
 	rtrim(c.ucr_code) ucr_code,
 	upper(u.ucr_categ) ucr_cat,
-	upper(u.ucr_group) ucr_group,
+	rtrim(u.ucr_group) ucr_group,
+	rtrim(replace(u.ucr_desc, u.ucr_group + ' - ', '')) ucr_desc,
 	rtrim(l.reportedas) reported_as,
 	(select rtrim(descriptn) from RMSReporting.dbo.systab2 x where x.code_agcy = l.csstatus and x.code_key = 'CASS') case_status,
-	l.csstatus, 
+	rtrim(l.csstatus) csstatus, 
 	(select case when x.descriptn = 'NO BIAS' then null else x.descriptn end from RMSReporting.dbo.systab2 x where c.hatebias = x.code_agcy and x.code_key = 'MOTI') bias,
 	isnull( (
 		select sex, rtrim(name_type) type
